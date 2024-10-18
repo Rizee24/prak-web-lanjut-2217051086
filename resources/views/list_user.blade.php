e6787e34d232c6e14f4115c7cb066441660c003a
Rizky Arrasyid <rizkyarrasyid525@gmail.com>
2024-10-18 22:24:05 +0700

    modultujuh

--- resources/views/list_user.blade.php
+++ resources/views/list_user.blade.php
@@ -9,7 +9,7 @@
                     <h4 class="mb-0 font-weight-bold">List User</h4>
                 </div>
                 <div class="card-body p-4">
-                <a href="{{ route('user.create') }}" class="btn btn-primary mb-3">Tambah Pengguna Baru</a>
+                    <a href="{{ route('user.create') }}" class="btn btn-primary mb-3">Tambah Pengguna Baru</a>
                     <div class="table-responsive">
                         <table class="table table-striped table-hover align-middle text-center">
                             <thead class="thead-dark">
@@ -18,6 +18,7 @@
                                     <th scope="col">Nama</th>
                                     <th scope="col">NPM</th>
                                     <th scope="col">Kelas</th>
+                                    <th scope="col">Aksi</th> <!-- Menambahkan kolom untuk aksi -->
                                 </tr>
                             </thead>
                             <tbody>
@@ -27,7 +28,15 @@
                                         <td>{{ $user->nama }}</td>
                                         <td>{{ $user->npm }}</td>
                                         <td>{{ $user->nama_kelas }}</td>
-                                        <td><a href="{{ route('user.show', $user->id) }}" class="btn btn-warning mb-3">Detail</a></td>
+                                        <td>
+                                            <a href="{{ route('user.edit', $user->id) }}" class="btn btn-warning">Edit</a>
+                                            <a href="{{ route('user.show', $user->id) }}" class="btn btn-info">Detail</a>
+                                            <form action="{{ route('user.destroy', $user->id) }}" method="POST" style="display:inline;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus user ini?')">Hapus</button>
+                                            </form>
+                                        </td>
                                     </tr>
                                 @endforeach
                             </tbody>
--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,12 @@ Route::post('/user/store', [UserController::class, 'store'])->name('user.store')
 
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
 
-Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
+// Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
+
+Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+
+Route::get('/user/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
+
+Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+Route::get('/user/{id}', [UserController::class, 'show'])->name('user.show');
